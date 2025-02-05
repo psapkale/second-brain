3227f26b2a83b454df7f36f8004be3fcb8b0e1ed
Prem <prempravinsapkale@gmail.com>
2025-02-05 22:16:37 +0530

    hook for userData

---
+++ fe/src/hooks/useUser.ts
@@ -0,0 +1,42 @@
+import _ from "lodash";
+
+export interface UserData {
+   name: string;
+   email: string;
+   imgUrl: string;
+   token: string;
+   createdAt: number;
+}
+
+export const useUser = () => {
+   const getUser = () => {
+      const userData = localStorage.getItem("userData");
+
+      return userData ? JSON.parse(userData) : null;
+   };
+
+   const udpateUserToken = (token: string) => {
+      const userData = getUser();
+
+      if (userData) {
+         localStorage.setItem(
+            "userData",
+            JSON.stringify({ ...userData, token, createdAt: _.now() })
+         );
+      }
+   };
+
+   const setUser = (userData: UserData) => {
+      return localStorage.setItem("userData", JSON.stringify(userData));
+   };
+
+   const getToken = () => {
+      const userData = localStorage.getItem("userData");
+
+      return userData ? JSON.parse(userData)?.token : null;
+   };
+
+   const removeUser = () => localStorage.removeItem("userData");
+
+   return { getUser, udpateUserToken, setUser, getToken, removeUser };
+};
