c36858262924b2bb4a658fa9008382bb17efd200
Prem <prempravinsapkale@gmail.com>
2025-02-24 16:38:47 +0530

    build fix

--- fe/src/context/themeContext.tsx
+++ fe/src/context/themeContext.tsx
@@ -9,7 +9,7 @@ interface ThemeContextType {
 
 const ThemeContext = createContext<ThemeContextType>({
    theme: "dark",
-   setTheme: (theme: Theme) => {},
+   setTheme: () => {},
 });
 
 export const useTheme = () => useContext(ThemeContext);
